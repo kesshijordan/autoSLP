e513572ed6cff33abf7827baf8c69aa03a23aee2
Ubuntu <ubuntu@ip-172-31-7-54.us-west-1.compute.internal>
2018-10-09 20:43:33 +0000

    added config attempting to get nginx to work

--- SLP_App/islp/__init__.py
+++ SLP_App/islp/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 # app.config['UPLOAD_FOLDER'] = 'tmp/'
 app.config['UPLOAD_FOLDER'] = 'islp/static/images/'
-app.secret_key = 'My secret key'
-
+app.secret_key = 'asksk8gklsjjjry'
+app.config['MAX_CONTENT_LENGTH'] = 20*1024*1024
 # we want to keep this at bottom to avoid circular imports
 from islp import views
